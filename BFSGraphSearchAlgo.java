0d4acbf2999ebe323bbe22e31d6f4cfd8469afba
NguyenDucPhuc4903 <146728794+NguyenDucPhuc4903@users.noreply.github.com>
2023-10-12 20:44:45 +0700

    Update BFSGraphSearchAlgo.java

--- BFSGraphSearchAlgo.java
+++ BFSGraphSearchAlgo.java
@@ -31,15 +31,24 @@ public class BFSGraphSearchAlgo implements ISearchAlgo {
 	@Override
 	public Node execute(Node root, String start, String goal) {
 		Queue<Node> frontier = new LinkedList<Node>();
+		boolean check = false;
 		frontier.add(root);
 		List<Node> explored = new ArrayList<Node>();
 		while (!frontier.isEmpty()) {
 			Node current = frontier.poll();
-			if(current.getLabel().equals(goal)) {
-				explored.add(current);
-				return current;
+			if(current.getLabel().equals(start)) {
+				check = true;
+				frontier.clear();
+				explored.clear();
+				current.setParent(null);
+
+				
+				}else if(current.getLabel().equals(goal)&&check==true) {
+					explored.add(current);
+					return current;
 				}
-			List<Edge> children = current.getChildren();
+//			explored.add(current);
+			List<Node> children = current.getChildrenNodes();
 			for (Node node : children) {
 				if(!frontier.contains(node)&&!explored.contains(node)) {
 					frontier.add(node);
